c558c19958e9fdb4dd78faee3265c47034a23d40
yntcdd <michaelzhang935@gmail.com>
2026-06-17 21:30:41 -0400

    80

---
+++ 80.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() < 2) {
+            return 1;
+        }
+        int k = 2;
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[k-2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
