e6bf9ccbf04704c04f114b49c81dffec848d4f29
yntcdd <michaelzhang935@gmail.com>
2026-06-19 21:10:04 -0400

    189

---
+++ 189.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        Reverse(nums, 0, n - 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, n - 1);
+    }
+    void Reverse(vector<int>& nums, int s, int e) {
+        while (e > s) {
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+            s++;
+            e--;
+        }
+    }
+};
