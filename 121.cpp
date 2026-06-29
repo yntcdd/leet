b073f131e1c16edb6e4339a19412900f93107891
yntcdd <michaelzhang935@gmail.com>
2026-06-28 20:54:31 -0400

    121

--- 121.cpp
+++ 121.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX;
+    int maxProfit(vector<int>& prices) { 
         int maxProfit = 0;
-        for (int price : prices) {
-            minPrice = min(minPrice, price);
-            maxProfit = max(maxProfit, price - minPrice);
+        for (int i = prices.size() - 1; i > 0; i--) {
+            if (prices[i] > prices[i-1]) {
+                maxProfit += prices[i] - prices[i-1];
+            }
         }
         return maxProfit;
     }
