289456429924927d33d49d1a1005abe028ac9577
yntcdd <michaelzhang935@gmail.com>
2026-06-29 20:12:23 -0400

    55

--- 55.cpp
+++ 55.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if (nums.size() == 1) {
-            return true;
-        }
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                bool found = false;
-                for (int j = i; j >= 0; j--) {
-                    if (nums[j] + j > i || nums[j] + j + 1>= nums.size()) {
-                        found = true;
-                        break;
-                    }
-                }
-                if (found == false) {
-                    return false;
+            if (nums[i]) continue;
+
+            bool ok = false;
+            for (int j = i; j >= 0; j--) {
+                if (j + nums[j] > i || j + nums[j] >= nums.size() - 1) {
+                    ok = true;
+                    break;
                 }
             }
+            if (!ok) return false;
         }
         return true;
     }
